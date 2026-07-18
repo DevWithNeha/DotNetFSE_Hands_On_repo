ea9c7a72caf000e08453302c0931aabd18616949
nehasingh148 <ranikumarisingh2004@gmail.com>
2026-07-19 01:41:15 +0530

    Add project files.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj1 = Singleton.GetInstance();
+
+            Singleton obj2 = Singleton.GetInstance();
+
+            obj1.ShowMessage();
+
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("Both objects are same.");
+            }
+            else
+            {
+                Console.WriteLine("Objects are different.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+            Console.WriteLine("Singleton Object Created");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello from Singleton Object");
+        }
+    }
+}
