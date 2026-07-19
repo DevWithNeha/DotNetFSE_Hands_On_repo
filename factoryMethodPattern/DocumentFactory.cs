4e321e10afa1fde609fbee204ea52eeb52118f57
nehasingh148 <ranikumarisingh2004@gmail.com>
2026-07-19 14:51:10 +0530

    Week 1 - Exercise 2 Factory Method Pattern

---
+++ factoryMethodPattern/DocumentFactory.cs
@@ -0,0 +1,18 @@
+﻿using FactoryMethodPattern;
+
+namespace factoryMethodPattern
+{
+    public class DocumentFactory
+    {
+        public static IDocument CreateDocument(string type)
+        {
+            if (type == "PDF")
+                return new PdfDocument();
+
+            if (type == "WORD")
+                return new WordDocument();
+
+            return null;
+        }
+    }
+}
---
+++ factoryMethodPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using FactoryMethodPattern;
+using System;
+
+namespace factoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDocument pdf = DocumentFactory.CreateDocument("PDF");
+            pdf.Open();
+
+            IDocument word = DocumentFactory.CreateDocument("WORD");
+            word.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
