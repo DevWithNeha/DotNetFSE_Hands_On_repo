8d5e058adc6ab81ba7db7200e64328cb190405d9
nehasingh148 <ranikumarisingh2004@gmail.com>
2026-07-19 15:09:56 +0530

    Week 1 - Exercise 3 Builder Pattern

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+
+            Console.WriteLine("Gaming Computer");
+            ComputerBuilder gaming = new GamingComputerBuilder();
+            director.Construct(gaming);
+            gaming.GetComputer().Show();
+
+            Console.WriteLine("Office Computer");
+            ComputerBuilder office = new OfficeComputerBuilder();
+            director.Construct(office);
+            office.GetComputer().Show();
+
+            Console.ReadKey();
+        }
+    }
+}
